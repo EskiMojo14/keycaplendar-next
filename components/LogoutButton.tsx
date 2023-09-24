2e5a6fc1104c79faf5eaf85ecca6e96a2f9eeab5
Ben Durrant <ben.j.durrant@gmail.com>
2023-09-24 03:36:58 +0100

    add type submit to logout button

--- components/LogoutButton.tsx
+++ components/LogoutButton.tsx
@@ -3,7 +3,7 @@ import Button from "./md/button";
 export default function LogoutButton() {
   return (
     <form action="/auth/sign-out" method="post">
-      <Button icon="logout" variant="filled-tonal">
+      <Button icon="logout" variant="filled-tonal" type="submit">
         Log out
       </Button>
     </form>
