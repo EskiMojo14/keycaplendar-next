955d2f9dd4f20d190ed35d35cbf39ec4b876c106
ben.durrant <ben.durrant@actual-experience.com>
2023-09-28 11:42:41 +0100

    add logout page

---
+++ app/logout/page.tsx
@@ -0,0 +1,13 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function Logout() {
+  const supabase = createServerComponentClient({ cookies });
+
+  await supabase.auth.signOut();
+
+  redirect("/login");
+
+  return null;
+}
