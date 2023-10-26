338a2782a9e6fa06a8ec3c0b524ecebb1dfadbdc
Ben Durrant <ben.j.durrant@gmail.com>
2023-10-26 19:09:22 +0100

    convert signout to a form action

--- components/keysets/logout-button.tsx
+++ components/keysets/logout-button.tsx
@@ -1,8 +1,19 @@
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 import Button from "../govuk/button";
+import { route } from "@/logic/lib/route";
 
 export default function LogoutButton() {
+  async function signOut() {
+    "use server";
+    const supabase = createRouteHandlerClient({ cookies });
+
+    await supabase.auth.signOut();
+    redirect(route("/login"));
+  }
   return (
-    <form action="/auth/sign-out" method="post">
+    <form action={signOut}>
       <Button type="submit">Log out</Button>
     </form>
   );
