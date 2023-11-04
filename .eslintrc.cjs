ae264622514caa9830db402d4d7191de6f1e4a06
ben.durrant <ben.durrant@actual-experience.com>
2023-11-04 20:29:53 +0000

    tweak eslint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -57,7 +57,7 @@ module.exports = {
     ],
     "lodash/import-scope": "error",
     "@typescript-eslint/prefer-nullish-coalescing": [
-      "error",
+      "warning",
       {
         ignoreConditionalTests: true,
         ignoreTernaryTests: true,
